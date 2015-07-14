f07724bf35570ffa1a1e615bf3a1ab238c224e24
uhyo <uhyo@uhyohyo.net>
2015-07-14 14:07:44 +0900

    client side ws

---
+++ actions/connect.js
@@ -0,0 +1,28 @@
+var Reflux=require('reflux');
+
+//connect to server.
+var connect= Reflux.createAction();
+
+connect.listen(function(params){
+    //connect to server
+    var p=location.pathname;
+    if(!/\/$/.test(p)){
+        p+="/";
+    }
+    var ws=new WebSocket(location.protocol.replace("http","ws")+"//"+location.host+p+"ws");
+    ws.onopen=function(e){
+        console.log("WebSocket connection is open");
+        ws.send("foo");
+    };
+    ws.onerror=function(e){
+        console.error("WebSocket error",e);
+    };
+    ws.onclose=function(e){
+        console.log("WebSocket connection closed by the server");
+    };
+    ws.onmessage=function(e){
+        console.log("Message from server: ",e.data);
+    };
+});
+
+module.exports = connect;
--- jsx/app/index.jsx
+++ jsx/app/index.jsx
@@ -1,7 +1,12 @@
 var React=require('react');
 
+var connect=require('../../actions/connect');
+
 module.exports = React.createClass({
     displayName:"App",
+    componentDidMount:function(){
+        connect();
+    },
     render:function(){
         return (<p>
             Hi
